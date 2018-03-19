20ad75588f451cbda6abb6c5dc5a55f5fed96f35
wellwind <wellwind@mail2000.com.tw>
2018-03-19 21:41:25 +0800

    Setting the root path

--- cypress/integration/app_spec.ts
+++ cypress/integration/app_spec.ts
@@ -3,4 +3,9 @@ describe('app', () => {
     cy.visit('http://localhost:4200');
     cy.get('app-root h1').contains('Welcome to app!');
   });
+
+  it('loads by root path', () => {
+    cy.visit('/');
+    cy.get('app-root h1').contains('Welcome to app!');
+  })
 });
