349777bb80900582774e6e452763549bc0ffe3a3
wellwind <wellwind@mail2000.com.tw>
2018-03-19 22:31:30 +0800

    Add given/when/then types to let step definitions support typescript

--- cypress/support/step_definitions/angular-app.ts
+++ cypress/support/step_definitions/angular-app.ts
@@ -2,6 +2,6 @@ given('I go to the first page', () => {
   cy.visit('/');
 })
 
-then('There should be a header named {string}', (title) => {
+then('There should be a header named {string}', (title, title2) => {
   cy.get('app-root h1').contains(title);
 })
---
+++ cypress/typings.d.ts
@@ -0,0 +1,3 @@
+declare var given: (description: string, callback: (...parameters) => void) => void;
+declare var when: (description: string, callback: (...parameters) => void) => void;
+declare var then: (description: string, callback: (...parameters) => void) => void;
